4b868402dff900517a6f5f980c2519e380aea217
hollyyuqizheng <holly_zyq@126.com>
2020-05-26 10:56:34 -0400

    add introduction and basic info section, add headshot image

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -13,16 +13,16 @@
 // limitations under the License.
 
 /**
- * Adds a random greeting to the page.
+ * Adds a random hobby to the page.
  */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+function addRandomHobby() {
+  const hobbies =
+      ['Clarinet', 'books', 'español'];
 
   // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  const hobby = hobbies[Math.floor(Math.random() * hobbies.length)];
 
   // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
+  const hobbyContainer = document.getElementById('hobby-container');
+  hobbyContainer.innerText = hobby;
 }
