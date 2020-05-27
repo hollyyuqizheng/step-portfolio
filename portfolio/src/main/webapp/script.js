35d5503d1dc41dfa0ac3a38b7d3af46bb9e7b746
hollyyuqizheng <holly_zyq@126.com>
2020-05-27 11:33:47 -0400

    1) add popup boxes for project description, these boxes are created in the onClick functions 2) add hover-over effect for headshot image 3) change headshot image to city skyline

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -12,8 +12,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+
+
 /**
- * Adds a random hobby to the page.
+ * Adds a random hobby to the page by displaying the hobby's description text
+ * and updating the wrapper's background color so that it appears 
  */
 function addRandomHobby() {
   const hobbies =
@@ -25,4 +28,43 @@ function addRandomHobby() {
   // Add it to the page.
   const hobbyContainer = document.getElementById('hobby-container');
   hobbyContainer.innerText = hobby;
+
+  const hobbyWrapper = document.getElementById('hobby-wrapper');
+  hobbyWrapper.style.backgroundColor = 'antiquewhite';
+}
+
+
+/**
+* This function is called when any of the project box is clicked.
+* Input: string -- name of the selected project
+* Results: 
+* - change the text of the project description box so that the description text appears. 
+* - change the background color of the description box so that it seems like the box appears
+* - If the project doesn't exist, a general "something is wrong" is displayed in the description box 
+*/
+function addProjectDescription(project) {
+
+    const details = {
+        nlp: 'Building language models',
+        ta: 'Held weekly office hours and review sessions for introductory Data Structure and Algorithms class',
+        helmet: 'Integrated speech command to a \"smart\" bike helmet built by a team of 9 friends',
+        unknown: 'Something is wrong, project doesn\'t exist'
+    };
+
+    const colors = {
+        nlp: '#6ccfe0',
+        ta: '#f7d36f',
+        helmet: '#e0b9f0',
+        unknown: 'black'
+    };
+
+    if (! (project in details)) {
+        project = 'unknown'; 
+    };
+
+    const wrapperName = 'project-description-';
+    const wrapper = document.getElementById(wrapperName.concat(project));
+
+    wrapper.innerText = details[project]; 
+    wrapper.style.backgroundColor = colors[project];
 }
